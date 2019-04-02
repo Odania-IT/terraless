6147fbb603984468f16e13bc508ffce10fe7517b
Mike Petersen <mike@odania-it.de>
2019-04-02 22:56:27 +0200

    Added test

--- terraless_provider_aws/templates-cloudfront_test.go
+++ terraless_provider_aws/templates-cloudfront_test.go
@@ -30,3 +30,30 @@ func TestTemplatesFunctions_RenderUploadTemplates(t *testing.T) {
 	assert.Contains(t, buffer.String(), `resource "aws_cloudwatch_log_group" "lambda-log-terraless-lambda-cloudfront"`)
 	assert.Contains(t, buffer.String(), `resource "aws_lambda_function" "terraless-lambda-cloudfront"`)
 }
+
+func TestTemplatesFunctions_RenderUploadTemplates_WithDomain(t *testing.T) {
+	// given
+	buffer := bytes.Buffer{}
+	config := schema.TerralessConfig{
+		ProjectName: "DummyProjectName",
+		Uploads: []schema.TerralessUpload{
+			{
+				Type: "s3",
+				Cloudfront: schema.TerralessCloudfront{
+					Domain: "my-dummy-domain.org",
+				},
+			},
+		},
+	}
+
+	// when
+	buffer = RenderUploadTemplates(config, buffer)
+
+	// then
+	assert.Contains(t, buffer.String(), `## Terraless Lambda@Edge`)
+	assert.Contains(t, buffer.String(), `resource "aws_cloudwatch_log_group" "lambda-log-terraless-lambda-cloudfront"`)
+	assert.Contains(t, buffer.String(), `resource "aws_lambda_function" "terraless-lambda-cloudfront"`)
+	assert.Contains(t, buffer.String(), `resource "aws_cloudfront_origin_access_identity" "terraless-default"`)
+	assert.Contains(t, buffer.String(), `resource "aws_cloudfront_distribution" "terraless-default"`)
+	assert.Contains(t, buffer.String(), `resource "aws_route53_record" "terraless-cloudfront-target-my-dummy-domain-org"`)
+}
