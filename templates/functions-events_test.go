8d2f6e29b5790854174460376d3b7acff677e5e9
Mike Petersen <mike@odania-it.de>
2019-04-04 23:20:04 +0200

    Updated flappy test

--- templates/functions-events_test.go
+++ templates/functions-events_test.go
@@ -13,8 +13,8 @@ func TestFunctionsEvents_ConsolidateEventData(t *testing.T) {
 		Config: schema.TerralessConfig{
 			Functions: map[string]schema.TerralessFunction{
 				"DummyFunction": {
-					Type: "aws",
-					Handler: "dummy.handler",
+					Type:        "aws",
+					Handler:     "dummy.handler",
 					Description: "My description",
 					Events: []schema.TerralessFunctionEvent{
 						{
@@ -24,8 +24,8 @@ func TestFunctionsEvents_ConsolidateEventData(t *testing.T) {
 					},
 				},
 				"OtherType": {
-					Type: "gcloud",
-					Handler: "dummy.handler",
+					Type:        "gcloud",
+					Handler:     "dummy.handler",
 					Description: "My description",
 					Events: []schema.TerralessFunctionEvent{
 						{
@@ -35,20 +35,20 @@ func TestFunctionsEvents_ConsolidateEventData(t *testing.T) {
 					},
 				},
 				"SpecificFunction": {
-					Type: "aws",
-					Handler: "specific.Handler",
-					Runtime: "dummyRuntime",
+					Type:       "aws",
+					Handler:    "specific.Handler",
+					Runtime:    "dummyRuntime",
 					MemorySize: 512,
-					Timeout: 60,
+					Timeout:    60,
 					Events: []schema.TerralessFunctionEvent{
 						{
-							Type: "http",
+							Type:   "http",
 							Method: "GET",
-							Path: "dummy/specific",
+							Path:   "dummy/specific",
 						},
 						{
 							Type: "sqs",
-							Arn: "arn:aws::::sqs",
+							Arn:  "arn:aws::::sqs",
 						},
 					},
 				},
@@ -60,50 +60,52 @@ func TestFunctionsEvents_ConsolidateEventData(t *testing.T) {
 	functionEvents := consolidateEventData(terralessData)
 
 	// then
-	expected := map[string]schema.FunctionEvents{
-		"aws": {
-			Events: map[string][]schema.FunctionEvent{
-				"http": {
-					{
-						FunctionName: "DummyFunction",
-						FunctionEvent: schema.TerralessFunctionEvent{
-							Type: "http",
-							Path: "dummy",
-						},
-					},
-					{
-						FunctionName: "SpecificFunction",
-						FunctionEvent: schema.TerralessFunctionEvent{
-							Type: "http",
-							Method: "GET",
-							Path: "dummy/specific",
-						},
-					},
-				},
-				"sqs": {
-					{
-						FunctionName: "SpecificFunction",
-						FunctionEvent: schema.TerralessFunctionEvent{
-							Type: "sqs",
-							Arn: "arn:aws::::sqs",
-						},
-					},
-				},
-			},
-		},
-		"gcloud": {
-			Events: map[string][]schema.FunctionEvent{
-				"http": {
-					{
-						FunctionName: "OtherType",
-						FunctionEvent: schema.TerralessFunctionEvent{
-							Type: "http",
-							Path: "dummy",
-						},
-					},
+	expectedGcloud := map[string][]schema.FunctionEvent{
+		"http": {
+			{
+				FunctionName: "OtherType",
+				FunctionEvent: schema.TerralessFunctionEvent{
+					Type: "http",
+					Path: "dummy",
 				},
 			},
 		},
 	}
-	assert.Equal(t, expected, functionEvents)
+	assert.Equal(t, 2, len(functionEvents))
+	assert.Equal(t, expectedGcloud, functionEvents["gcloud"].Events)
+
+	expectedAwsSqs := []schema.FunctionEvent{
+		{
+			FunctionName: "SpecificFunction",
+			FunctionEvent: schema.TerralessFunctionEvent{
+				Type: "sqs",
+				Arn:  "arn:aws::::sqs",
+			},
+		},
+	}
+	assert.Equal(t, expectedAwsSqs, functionEvents["aws"].Events["sqs"])
+	expectedAwsHttp1 := schema.FunctionEvent{
+		FunctionName: "DummyFunction",
+		FunctionEvent: schema.TerralessFunctionEvent{
+			Type: "http",
+			Path: "dummy",
+		},
+	}
+	expectedAwsHttp2 := schema.FunctionEvent{
+		FunctionName: "SpecificFunction",
+		FunctionEvent: schema.TerralessFunctionEvent{
+			Type:   "http",
+			Method: "GET",
+			Path:   "dummy/specific",
+		},
+	}
+
+	awsHttpEvents := functionEvents["aws"].Events["http"]
+	if awsHttpEvents[0].FunctionName == "DummyFunction" {
+		assert.Equal(t, expectedAwsHttp1, awsHttpEvents[0])
+		assert.Equal(t, expectedAwsHttp2, awsHttpEvents[1])
+	} else {
+		assert.Equal(t, expectedAwsHttp2, awsHttpEvents[0])
+		assert.Equal(t, expectedAwsHttp1, awsHttpEvents[1])
+	}
 }
